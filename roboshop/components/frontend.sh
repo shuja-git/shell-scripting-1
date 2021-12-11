eee28168b64efe2a0b33556e6f32ac5449753a80
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-11 07:39:54 +0530

    Final spacing pushed

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,6 +2,7 @@
 
 # source is nothing but import , like export command
 source components/common.sh
+MAX_LENGTH=$(cat ${0}  | grep -v cat | grep STAT_CHECK | awk -F '"' '{print $2}'  | awk '{ print length }'  | sort  | tail -1)
 
 yum install nginx -y &>>${LOG_FILE}
 STAT_CHECK $? "Nginx Installation"
