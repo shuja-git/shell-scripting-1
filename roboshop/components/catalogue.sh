ff45bd278e7f1f893d7e3bdf21add706e7585cf9
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-13 07:01:03 +0530

    Catalogue Steps

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,7 +4,9 @@ source components/common.sh
 
 MAX_LENGTH=$(cat ${0}  | grep -v -w cat | grep STAT_CHECK | awk -F '"' '{print $2}'  | awk '{ print length }'  | sort  | tail -1)
 
-echo $MAX_LENGTH
+if [ $MAX_LENGTH -lt 24 ];then
+  MAX_LENGTH=24
+fi
 
 yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
 STAT_CHECK $? "Install NodeJS"
