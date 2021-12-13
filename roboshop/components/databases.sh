11e063cf8fd2a438d3ce404dd2c84fd9dceb185c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-13 06:11:20 +0530

    MySQL Steps

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -63,3 +63,45 @@ fi
 
 rabbitmqctl set_user_tags roboshop administrator  &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${LOG_FILE}
 STAT_CHECK $? "Configure APp User Permissions"
+
+### MySQL Setup
+
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
+STAT_CHECK $? "Configure YUM Repos"
+
+
+yum install mysql-community-server -y &>>${LOG_FILE}
+STAT_CHECK $? "Installing MySQL"
+
+systemctl enable mysqld &>>${LOG_FILE} && systemctl start mysqld &>>${LOG_FILE}
+STAT_CHECK $? "Start MySQL Service"
+
+
+#
+#Now a default root password will be generated and given in the log file.
+## grep temp /var/log/mysqld.log
+#
+#Next, We need to change the default root password in order to start using the database service.
+## mysql_secure_installation
+#
+#You can check the new password working or not using the following command.
+#
+## mysql -u root -p
+#
+#Run the following SQL commands to remove the password policy.
+#> uninstall plugin validate_password;
+#Setup Needed for Application.
+#As per the architecture diagram, MySQL is needed by
+#
+#Shipping Service
+#So we need to load that schema into the database, So those applications will detect them and run accordingly.
+#
+#To download schema, Use the following command
+#
+## curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+#Load the schema for Services.
+#
+## cd /tmp
+## unzip mysql.zip
+## cd mysql-main
+## mysql -u root -pRoboShop@1 <shipping.sql
