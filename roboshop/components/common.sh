ff2cc1ceaad88545c56d2d0305a516e1c2622500
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-13 07:31:06 +0530

    Update cart steps

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -54,9 +54,12 @@ NODEJS() {
 
   chown roboshop:roboshop -R /home/roboshop
 
-  sed -i -e 's/MONGO_DNSNAME/mongo.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' /home/roboshop/${component}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${component}/systemd.service /etc/systemd/system/${component}.service  &>>${LOG_FILE}
+  sed -i  -e 's/MONGO_DNSNAME/mongo.roboshop.internal/' \
+          -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
+          -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' \
+          -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \ /home/roboshop/${component}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${component}/systemd.service /etc/systemd/system/${component}.service  &>>${LOG_FILE}
   STAT_CHECK $? "Update SystemD Config file"
 
   systemctl daemon-reload &>>${LOG_FILE} && systemctl start ${component} &>>${LOG_FILE} && systemctl enable ${component} &>>${LOG_FILE}
-  STAT_CHECK $? "Start Catalogue Service"
+  STAT_CHECK $? "Start ${component} Service"
 }
