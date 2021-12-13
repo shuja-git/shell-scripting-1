f3add2509ba9bbdf6ea08abbd0cc2168eec0f09d
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-13 06:57:29 +0530

    Catalogue Steps

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 
-MAX_LENGTH=$(cat ${0}  components/common.sh | grep -v -w cat | grep STAT_CHECK | awk -F '"' '{print $2}'  | awk '{ print length }'  | sort  | tail -1)
+MAX_LENGTH=$(cat ${0}  | grep -v -w cat | grep STAT_CHECK | awk -F '"' '{print $2}'  | awk '{ print length }'  | sort  | tail -1)
 
 yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
 STAT_CHECK $? "Install NodeJS"
