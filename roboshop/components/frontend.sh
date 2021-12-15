6e0e82b8e11b6ecf327fb4670c0dbff66781d35d
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-15 06:44:37 +0530

    Update shell-scripting/

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -17,11 +17,9 @@ STAT_CHECK $? "Copying Frontend Content"
 cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $? "Copy Nginx Config File"
 
-sed -i  -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' \
-        -e '/cart/ s/localhost/cart.roboshop.internal/' \
-        -e '/user/ s/localhost/user.roboshop.internal/' \
-        -e '/shipping/ s/localhost/shipping.roboshop.internal/' \
-        -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+for component in catalogue cart user shipping payment ; do
+  sed -i -e "/${component}/ s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done
 STAT_CHECK $? "Update Nginx Config File"
 
 systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
